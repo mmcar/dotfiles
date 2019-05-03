1188b5a1b216236df5c90c7ccb60fc7acf2a894f
Matthew McArthur <matthew.mcarthur91@gmail.com>
2019-05-03 18:40:10 -0300

    config.fish now bootstraps fisher if not set-up

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,9 @@ set PATH $HOME/.rbenv/shims $PATH
 set PATH $HOME/bin/scripts $PATH
 rbenv rehash >/dev/null ^&1
 
+# bootstrap fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
