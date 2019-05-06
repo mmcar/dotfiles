bfd7346dd15bed4f92106260f46fcaa5391d8727
Matthew McArthur <matthew.mcarthur91@gmail.com>
2019-05-06 00:20:01 -0300

    migrated some scripts from legacy folder

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,5 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+
+# TODO: automatically install fisher packages if they aren't installed
